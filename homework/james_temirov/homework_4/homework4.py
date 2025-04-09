f7de8319c752c35f981d073bba3021176c5b3e0e
James Temirov <temirovj@gmail.com>
2025-04-09 03:13:36 -0400

    Homework_4

--- homework/james_temirov/homework_4/homework4.py
+++ homework/james_temirov/homework_4/homework4.py
@@ -1,5 +1,5 @@
 my_dict = {
-    'tuple':(1, 2, 3, 4, 5),
+    'tuple': (1, 2, 3, 4, 5),
     'list': [45, 657, 243, 6456, 7567, 'food', True, 'kitty'],
     'dict': {'name': 'James', 'last_name': 'Temirov', 'age': 30, 'state': 'NC', 'city': 'Charlotte', 'car': 'VW'},
     'set': {100, 200, 300, 400, 500, 600, 700}
@@ -11,7 +11,7 @@ my_dict['list'].append('Adding last item')
 my_dict['list'].pop(1)
 
 my_dict['dict'][('i am a tuple', )] = 'i hope its correct'
-#В примере вы специально оставили запятую? я вложил картеж в словарь получается?
+# В примере вы специально оставили запятую? я вложил картеж в словарь получается?
 my_dict['dict'].pop('state')
 
 my_dict['set'].add(800)
